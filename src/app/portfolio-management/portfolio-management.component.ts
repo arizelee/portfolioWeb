f80d60cd6934f68dafd1e4bc5e8161da3cf6a7f9
Arize Lee <6594322-arizelee@users.noreply.gitlab.com>
2021-08-01 22:47:08 -0400

    web skeleton

---
+++ src/app/portfolio-management/portfolio-management.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-management',
+  templateUrl: './portfolio-management.component.html',
+  styleUrls: ['./portfolio-management.component.scss']
+})
+export class PortfolioManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
